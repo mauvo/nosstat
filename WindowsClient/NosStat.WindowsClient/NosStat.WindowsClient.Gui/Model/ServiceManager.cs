8d980ba0fd48f8549084771bd3a9f473c4e38d5d
David Pond <david.pond@red-gate.com>
2015-02-17 18:44:35 +0000

    Wire-up Install button to ServiceManager.

--- WindowsClient/NosStat.WindowsClient/NosStat.WindowsClient.Gui/Model/ServiceManager.cs
+++ WindowsClient/NosStat.WindowsClient/NosStat.WindowsClient.Gui/Model/ServiceManager.cs
@@ -1,7 +1,38 @@
+using System;
+using System.Threading.Tasks;
+
 namespace NosStat.WindowsClient.Gui.Model
 {
     public class ServiceManager
     {
-        public bool IsInstalled { get {return false;} }
+        public bool IsInstalled { get; private set; } = false;
+
+        public void InstallService(Action onServiceInstallCompleteCallback, Action<Exception> onServiceInstallFailedCallback)
+        {
+            Task doingServiceInstall = DoServiceInstall();
+            doingServiceInstall.ContinueWith((t) => OnServiceInstallCompleteCallback(t, onServiceInstallCompleteCallback, onServiceInstallFailedCallback));
+        }
+
+        private Task DoServiceInstall()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                System.Threading.Thread.Sleep(1000);
+                IsInstalled = true;
+            });
+        }
+
+        private void OnServiceInstallCompleteCallback(Task task, Action onServiceInstallCompleteCallback, Action<Exception> onServiceInstallFailedCallback)
+        {
+            try
+            {
+                task.Wait();
+                Task.Factory.StartNew(onServiceInstallCompleteCallback);
+            }
+            catch (Exception e)
+            {
+                Task.Factory.StartNew(() => onServiceInstallFailedCallback(e));
+            }
+        }
     }
 }
--- WindowsClient/NosStat.WindowsClient/NosStat.WindowsClient.Gui/ViewModel/ServiceViewModel.cs
+++ WindowsClient/NosStat.WindowsClient/NosStat.WindowsClient.Gui/ViewModel/ServiceViewModel.cs
@@ -1,6 +1,8 @@
+using System;
 using System.ComponentModel;
 using System.Net;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using NosStat.WindowsClient.Gui.Annotations;
 using NosStat.WindowsClient.Gui.Model;
 
@@ -17,6 +19,32 @@ namespace NosStat.WindowsClient.Gui.ViewModel
             serviceManager = new ServiceManager();
         }
 
+        private ICommand m_InstallService;
+        public ICommand InstallService
+        {
+            get
+            {
+                if (m_InstallService == null)
+                    m_InstallService = CommandFactory.CreateCommand(ExecuteInstallService);
+                return m_InstallService;
+            }
+        }
+
+        private void ExecuteInstallService()
+        {
+            serviceManager.InstallService(OnServiceInstallComplete, OnServiceInstallFailed);
+        }
+
+        private void OnServiceInstallFailed(Exception exception)
+        {
+            OnPropertyChanged("StatusMessage");
+        }
+
+        private void OnServiceInstallComplete()
+        {
+            OnPropertyChanged("StatusMessage");
+        }
+
         public string StatusMessage
         {
             get
@@ -26,7 +54,6 @@ namespace NosStat.WindowsClient.Gui.ViewModel
                 return "Service is not installed.";
             }
         }
-
 
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
