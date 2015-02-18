a8ee5ae6f0778842d9a804099dafaa129f378248
David Pond <david.pond@red-gate.com>
2015-02-18 13:17:50 +0000

    Create test project. Test to see if we can detect if a Service is installed.

---
+++ WindowsClient/NosStat.WindowsClient.Tests/WindowsServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NosStat.WindowsClient.Tests
+{
+    [TestFixture]
+    public class WindowsServiceTests
+    {
+        [Test]
+        public void IsServiceInstalled()
+        {
+            var serviceName = "ThisDoesNotExist";
+            var service = ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == serviceName);
+            var isInstalled = service != null;
+
+            Assert.IsFalse(isInstalled);
+        }
+    }
+}
