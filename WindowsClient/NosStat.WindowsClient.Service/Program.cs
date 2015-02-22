f79c1f5e0ff4d0f90250767f9e0743406e4b5c99
David Pond <davidpond@gmail.com>
2015-02-22 13:18:50 +0000

    Install on /i, uninstall on /u

--- WindowsClient/NosStat.WindowsClient.Service/Program.cs
+++ WindowsClient/NosStat.WindowsClient.Service/Program.cs
@@ -12,14 +12,34 @@ namespace NosStat.WindowsClient.Service
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            // http://www.codeproject.com/Articles/21405/Windows-Services-Can-Install-Themselves
+            if (args != null && args.Length == 1 && args[0].Length > 1 && (args[0][0] == '-' || args[0][0] == '/'))
             {
+                switch (args[0].Substring(1).ToLower())
+                {
+                    default:
+                        break;
+                    case "install":
+                    case "i":
+                        SelfInstaller.InstallMe();
+                        break;
+                    case "uninstall":
+                    case "u":
+                        SelfInstaller.UninstallMe();
+                        break;
+                }
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
                 new NosgothLogMonitoringService()
-            };
-            ServiceBase.Run(ServicesToRun);
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
         }
     }
 }
