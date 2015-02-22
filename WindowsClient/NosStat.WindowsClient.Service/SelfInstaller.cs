a57e6c377b0e1da5ecc86da84d723462d6be06d8
David Pond <davidpond@gmail.com>
2015-02-22 13:14:49 +0000

    Added self installer to service http://www.codeproject.com/Articles/21405/Windows-Services-Can-Install-Themselves

---
+++ WindowsClient/NosStat.WindowsClient.Service/SelfInstaller.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Configuration.Install;
+
+namespace gotnet.biz.Utilities.WindowsServices
+{
+    /// <summary>
+    /// http://www.codeproject.com/Articles/21405/Windows-Services-Can-Install-Themselves
+    /// </summary>
+    public static class SelfInstaller
+    {
+        private static readonly string _exePath = Assembly.GetExecutingAssembly().Location;
+        public static bool InstallMe()
+        {
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[] { _exePath });
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool UninstallMe()
+        {
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[] { "/u", _exePath });
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
