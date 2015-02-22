4966677b940abb924714c3f88afe54e0656343f5
David Pond <davidpond@gmail.com>
2015-02-22 14:57:35 +0000

    Create simple interface for WCP communication between Windows Service and GUI

---
+++ WindowsClient/NosStat.WindowsClient.ServiceInterfaces/INosStatService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NosStat.WindowsClient.ServiceInterfaces
+{
+    [ServiceContract]
+    public interface INosStatService
+    {
+        [OperationContract]
+        bool RegisterForLogEvents(Action<string> logCallback);
+
+    }
+}
